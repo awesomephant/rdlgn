c41da4058718a84cb3d164501450f35c3c222a56
max <max@koehler-kn.de>
2013-03-04 18:19:15 +0100

    fixed pageview, made single-post view respond better.

--- single.php
+++ single.php
@@ -16,15 +16,25 @@ get_header(); ?>
 				<?php get_template_part( 'content', 'single' ); ?>
 
 
+			<?php endwhile; // end of the loop. ?>
+
+			</div><!-- #content .site-content -->
+		
+		</div><!-- #primary .content-area -->
+		<section class='secondary'>	
+		<section class='comments'>
 				<?php
 					// If comments are open or we have at least one comment, load up the comment template
 					if ( comments_open() || '0' != get_comments_number() )
 						comments_template( '', true );
 				?>
-
-			<?php endwhile; // end of the loop. ?>
-
-			</div><!-- #content .site-content -->
-		</div><!-- #primary .content-area -->
-
+			</section>
+		<section class='sidebar'>
+		<?php if ( ! dynamic_sidebar() ) : ?>
+            <li>{static sidebar item 1}</li>
+            <li>{static sidebar item 2}</li>
+        <?php endif; ?>
+		
+		</section>	
+		</section>
 <?php get_footer(); ?>
